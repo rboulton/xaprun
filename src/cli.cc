406a4de0d85c8caad92fc6420b31523ac90f3bfb
Richard Boulton <richard@tartarus.org>
2010-02-09 11:54:17 +0000

    Add basic interface to start up server

---
+++ src/cli.cc
@@ -0,0 +1,41 @@
+/** @file cli.cc
+ * @brief Command line interface for running the server.
+ */
+/*
+ * Copyright (c) 2010 Richard Boulton
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <config.h>
+
+#include "server.h"
+#include "settings.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+    ServerSettings settings;
+    int args_err = settings.parse_args(argc, argv);
+    if (args_err != -1) return args_err;
+    Server server(settings);
+    if (server.run()) return 0;
+    fprintf(stderr, "%s\n", server.get_error_message().c_str());
+    return 1;
+}
