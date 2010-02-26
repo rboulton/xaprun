57de21bb3a4edb97b752b813c23c1947998f221b
Richard Boulton <richard@tartarus.org>
2010-02-26 15:36:33 +0000

    Build system fixes to work with 1.1

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,11 @@
 
 # Nasty hack - if we have a xapian-config, try and guess where the macro is based on it.
 xapconfig=`which xapian-config`
+if [ "x$xapconfig"=="x" ];
+then
+    xapconfig=`which xapian-config-1.1`;
+fi
+
 xapmacropath=`cat $xapconfig | sed -n '/^prefix="/p' | sed 's/prefix=/-I /;s/"//g'`
 if [ "x$xapmacropath"=="x" ];
 then
