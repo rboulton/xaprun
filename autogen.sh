57e627d81e8c2cfc17814144316de8957ba74486
Richard Boulton <richard@tartarus.org>
2010-03-15 08:59:56 +0000

    Fix comparisons

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # Nasty hack - if we have a xapian-config, try and guess where the macro is based on it.
 xapconfig=`which xapian-config`
-if [ "x$xapconfig"=="x" ];
+if [ "x$xapconfig" == "x" ];
 then
     xapconfig=`which xapian-config-1.1`;
 fi
 
 xapmacropath=`cat $xapconfig | sed -n '/^prefix="/p' | sed 's/prefix=/-I /;s/"//g'`
-if [ "x$xapmacropath"=="x" ];
+if [ "x$xapmacropath" == "x" ];
 then
     xapmacropath="$xapmacropath/share/aclocal";
 fi
