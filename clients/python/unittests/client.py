da48a0b9e53aa7dc592b5d286b5d5e6b25782d89
Richard Boulton <richard@tartarus.org>
2010-03-15 18:48:54 +0000

    Add a (failing) test of insert

--- clients/python/unittests/client.py
+++ clients/python/unittests/client.py
@@ -10,3 +10,9 @@ class TestClient(unittest.TestCase):
     def test_client(self):
         c = xaprun.Client()
         self.assertEqual(c.version(), '0.1')
+
+    def test_insert(self):
+        c = xaprun.Client()
+        db = c.db('test1')
+        docid = db.insert({'text': 'hello world'})
+        self.assertNotEqual(docid, '')
