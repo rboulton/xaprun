9b1477019415ab061ba70b62372fd01d1330aedc
Richard Boulton <richard@tartarus.org>
2010-03-17 18:43:38 +0000

    Add license boilerplate, and timeouts for the connections

--- clients/python/unittests/client.py
+++ clients/python/unittests/client.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python
+# Copyright (c) 2010 Richard Boulton
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
 import unittest
 import xaprun
@@ -8,11 +26,11 @@ class TestClient(unittest.TestCase):
 
     """
     def test_client(self):
-        c = xaprun.Client()
+        c = xaprun.Client(timeout=5)
         self.assertEqual(c.version(), '0.1')
 
     def test_insert(self):
-        c = xaprun.Client()
+        c = xaprun.Client(timeout=5)
         db = c.db('test1')
         docid = db.insert({'text': 'hello world'})
         self.assertNotEqual(docid, '')
